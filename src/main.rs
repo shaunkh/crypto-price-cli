6002b229e17d9cd9cbd26a4ddcb2f9361ef16548
shaunkh <shaun.khundker@gmail.com>
2022-12-16 11:41:15 -0500

    add deps + api key

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let args: Vec<String> = env::args().collect();
+    let mut ticker: String = "ETH".to_string();
+
+    if args.len() < 2 {
+        println!("Since you didn't specify a crypto ticker, it is defaulted to ETH.");
+    } else {
+        ticker = args[1].clone();
+    }
+
+    let _coinmarketcap_api_key =
+        env::var("COINMARKETCAP_API_KEY").expect("COINMARKETCAP_API_KEY must be set.");
+
+    println!("{:?}", ticker);
 }
