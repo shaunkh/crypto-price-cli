3cad5ffe1b3d771544b2cbc50e19571393a47440
shaunkh <shaun.khundker@gmail.com>
2022-12-17 18:08:36 -0500

    like halfway?

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use dotenv::dotenv;
+use reqwest::header::{HeaderMap, AUTHORIZATION};
 use std::collections::HashMap;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let api_key = env::var("BREWS_API_KEY");
+    let api_token = env::var("BREWS_API_TOKEN");
+
+    let headers = HeaderMap::new();
+    headers.insert(AUTHORIZATION, "");
+
     let resp = reqwest::get("https://httpbin.org/ip")
         .await?
         .json::<HashMap<String, String>>()
