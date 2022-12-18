96801805a6b589af23a0c0af0db60a776fa9fcac
shaunkh <shaun.khundker@gmail.com>
2022-12-17 19:38:33 -0500

    use client

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 use dotenv::dotenv;
 use reqwest::header::{HeaderMap, AUTHORIZATION};
+use reqwest::Client;
 use std::collections::HashMap;
 use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
+    // let mut headers = HeaderMap::new();
+    // headers.insert(
+    //     AUTHORIZATION,
+    //     format!("Bearer {}", api_key).parse().unwrap(),
+    // );
 
-    let api_key = env::var("BREWS_API_KEY");
-    let api_token = env::var("BREWS_API_TOKEN");
+    let client = Client::new();
 
-    let headers = HeaderMap::new();
-    headers.insert(AUTHORIZATION, "");
-
-    let resp = reqwest::get("https://httpbin.org/ip")
+    let resp = client
+        .get("https://httpbin.org/ip")
+        .send()
         .await?
         .json::<HashMap<String, String>>()
         .await?;
     println!("{:#?}", resp);
+
     Ok(())
 }
